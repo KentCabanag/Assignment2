2c183d4935137ee4a5217e5f28571dcb230170f3
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2021-10-24 00:07:33 +0800

    commit

---
+++ Program2.py
@@ -0,0 +1,6 @@
+apple = int(input("Number of Apples you want to buy: "))
+orange = int(input("Number of Oranges you want to buy: "))
+t_apple = apple*20
+t_orange = orange*25
+total = t_apple + t_orange 
+print(f"The total ammount is {total}.")
