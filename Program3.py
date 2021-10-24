ad900dd453ee195183b1a26940ab26fc8a66bd16
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2021-10-24 17:08:06 +0800

    commit

--- Program3.py
+++ Program3.py
@@ -1,1 +1,5 @@
-ammount = int(input("Enter the ammount of money you have: "))
+ammount = float(input("Enter the ammount of money you have: "))
+aplprice = float(input("Enter the price of an apple: "))
+napple = int(ammount/aplprice)
+change = ammount % aplprice
+print(f"You can buy {napple} apples and your change is {change:.2f} pesos.")
